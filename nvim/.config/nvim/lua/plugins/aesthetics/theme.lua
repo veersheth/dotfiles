716a72e80552f516a230fa24241101af48c1b1d8
Veer <veerksheth@gmail.com>
2025-04-27 00:58:58 +1000

    nvim: vague theme

--- nvim/.config/nvim/lua/plugins/aesthetics/theme.lua
+++ nvim/.config/nvim/lua/plugins/aesthetics/theme.lua
@@ -1,3 +1,13 @@
+return {
+    "vague2k/vague.nvim",
+    config = function()
+        require("vague").setup({ transparent = true })
+        vim.cmd("colorscheme vague")
+        vim.cmd("hi Normal guibg=#111111")  
+    end
+}
+
+
 -- return {}
 
 -- return { "ellisonleao/gruvbox.nvim", priority = 1000,
@@ -15,28 +25,28 @@
 --   end,
 -- }
 
-return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			color_overrides = {
-				all = {
-					text = "#ffffff",
-				},
-				mocha = {
-					base = "#080808",
-					mantle = "#0f0f0f",
-					crust = "#080808",
-				},
-        latte = {},
-				frappe = {},
-				macchiato = {},
-			},
-		})
-
-		vim.cmd.colorscheme("catppuccin")
-	end,
-}
+-- return {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			flavour = "mocha",
+-- 			color_overrides = {
+-- 				all = {
+-- 					text = "#ffffff",
+-- 				},
+-- 				mocha = {
+-- 					base = "#080808",
+-- 					mantle = "#0f0f0f",
+-- 					crust = "#080808",
+-- 				},
+--         latte = {},
+-- 				frappe = {},
+-- 				macchiato = {},
+-- 			},
+-- 		})
+--
+-- 		vim.cmd.colorscheme("catppuccin")
+-- 	end,
+-- }
