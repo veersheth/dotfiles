25491c62ce044acc0d0c3f9bb073c9db1b1ab730
Veer <veerksheth@gmail.com>
2025-05-13 19:55:48 +1000

    waybar

---
+++ hypr/.config/waybar/scripts/media.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+artist=$(playerctl metadata artist 2>/dev/null)
+title=$(playerctl metadata title 2>/dev/null)
+status=$(playerctl status 2>/dev/null)
+
+if [[ "$status" == "Playing" ]]; then
+    icon=""  # Pause
+elif [[ "$status" == "Paused" ]]; then
+    icon=""  # Play
+else
+    echo '{"text": "", "class": "stopped"}'
+    exit
+fi
+
+echo "{\"text\": \"$icon   $artist - $title\", \"class\": \"$status\"}"
+
+# #!/bin/bash
+#
+# artist=$(playerctl metadata artist 2>/dev/null)
+# title=$(playerctl metadata title 2>/dev/null)
+# status=$(playerctl status 2>/dev/null)
+#
+# if [[ "$status" == "Playing" || "$status" == "Paused" ]]; then
+#     icon="⏵"
+#     [[ "$status" == "Paused" ]] && icon="⏸"
+#     echo "{\"text\": \"$icon $artist - $title\", \"class\": \"$status\"}"
+# else
+#     echo "{\"text\": \"\", \"class\": \"stopped\"}"
+# fi
+#
