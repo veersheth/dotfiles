634267843c6da35c599661bcc7db69c9cc7f4aff
Veer <veerksheth@gmail.com>
2025-05-21 12:14:46 +1000

    hypr: create hyprsunset script

--- hypr/.config/hypr/scripts/toggle-nightlight.sh
+++ hypr/.config/hypr/scripts/toggle-nightlight.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Check if redshift is running
-if pgrep -x "redshift" > /dev/null
-then
-    # If redshift is running, kill the process (turn off night light)
-    pkill redshift
-    echo "Night Light turned off"
+NIGHT_TEMP=4500
+
+if pgrep -x "hyprsunset" > /dev/null; then
+    killall hyprsunset
+    notify-send "Nightlight off"
 else
-    # If redshift is not running, start it (turn on night light)
-    redshift -O 4000
-    echo "Night Light turned on"
+    hyprsunset &
+    sleep 0.2
+    hyprctl hyprsunset temperature 4000
+    notify-send "Nightlight on"
 fi
+
+
 
