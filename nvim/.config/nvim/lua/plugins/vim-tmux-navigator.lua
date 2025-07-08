5106c3aba56e59cc6ace39498ef735f87528aee1
Veer <veerksheth@gmail.com>
2025-07-08 15:08:34 +1000

    tmux/vim: use alt to nav bw tmux and nivm

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,1 +1,18 @@
-return { "https://github.com/christoomey/vim-tmux-navigator" }
+return {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+    },
+    keys = {
+        { "<a-h>",  "<cmd>TmuxNavigateLeft<cr>" },
+        { "<a-j>",  "<cmd>TmuxNavigateDown<cr>" },
+        { "<a-k>",  "<cmd>TmuxNavigateUp<cr>" },
+        { "<a-l>",  "<cmd>TmuxNavigateRight<cr>" },
+        { "<a-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
+}
