2b8f8b4c71f9ade87b3501a92811644c422e8b1a
Veer <veerksheth@gmail.com>
2025-09-05 18:30:10 +1000

    nvim: writing mode centers text

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -17,20 +17,25 @@ vim.keymap.set("n", "<leader>w", function()
     writing_mode = not writing_mode
 
     if writing_mode then
-        vim.opt_local.wrapmargin = 6
+        vim.opt_local.wrapmargin = 30
         vim.opt_local.formatoptions:append("t")
         vim.opt_local.linebreak = true
         vim.opt_local.wrap = true
         vim.opt_local.spell = true
-        vim.notify("Writing mode enabled 😁👍", vim.log.levels.INFO)
+        -- fake margins
+        vim.opt_local.foldcolumn = "9"
+        vim.opt_local.signcolumn = "yes:9"
+        vim.notify("Writing mode enabled", vim.log.levels.INFO)
     else
-        -- Reset to defaults (adjust if needed)
         vim.opt_local.wrapmargin = 0
         vim.opt_local.formatoptions:remove("t")
         vim.opt_local.linebreak = false
         vim.opt_local.wrap = false
         vim.opt_local.spell = false
-        vim.notify("Writing mode disabled 😔🖕", vim.log.levels.INFO)
+        -- reset margins
+        vim.opt_local.foldcolumn = "0" 
+        vim.opt_local.signcolumn = "yes"
+        vim.notify("Writing mode disabled", vim.log.levels.INFO)
     end
 end, { desc = "Toggle writing mode" })
 
