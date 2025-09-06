67caa13f29b5adead8639fc869567732dbe1bbd8
Veer <veerksheth@gmail.com>
2025-09-06 18:06:09 +1000

    nvim: scope nvim to directory passed in args

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,3 +2,26 @@ require("options")
 require("autocmds.low-level-autocmds")
 require("autocmds.vim-sessions")
 require("lazy-config")
+
+-- if nvim is called on a directory, scope session to it
+local function find_start_dir()
+  for _, arg in ipairs(vim.v.argv) do
+    if not arg:match("^%-") and vim.fn.isdirectory(arg) == 1 then
+      return arg
+    end
+  end
+  return nil
+end
+
+local foldertogo = find_start_dir()
+
+if foldertogo then
+  local group = vim.api.nvim_create_augroup("FolderToGo", { clear = true })
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = group,
+    callback = function()
+      vim.cmd.cd(foldertogo)
+    end,
+  })
+end
+
