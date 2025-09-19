0701bbaab77ca64b24d2aecce2dc8157ded0a90d
Veer <veerksheth@gmail.com>
2025-09-19 14:52:43 +1000

    bloat

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+    -- 'nvim-lualine/lualine.nvim',
+    -- config = function()
+    --     require('lualine').setup {
+    --         options = {
+    --             icons_enabled = false,
+    --             theme = 'auto',
+    --             component_separators = { left = '', right = '' },
+    --             section_separators = { left = '', right = '' },
+    --             ignore_focus = {},
+    --             always_divide_middle = true,
+    --             always_show_tabline = true,
+    --             globalstatus = false,
+    --         },
+    --     }
+    -- end
+}
