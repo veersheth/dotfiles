8818c816c481789a7132492d8b76627dcbf14029
Veer <veerksheth@gmail.com>
2025-12-29 14:02:25 +0530

    chore

--- waybar/.config/waybar/scripts/media.sh
+++ waybar/.config/waybar/scripts/media.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-artist=$(playerctl metadata artist 2>/dev/null)
-title=$(playerctl metadata title 2>/dev/null)
-status=$(playerctl status 2>/dev/null)
+artist=$(playerctl --player=spotify metadata artist 2>/dev/null)
+title=$(playerctl --player=spotify metadata title 2>/dev/null)
+status=$(playerctl --player=spotify status 2>/dev/null)
 
 if [[ "$status" == "Playing" ]]; then
-    icon=""  # Pause
+    icon=""
 elif [[ "$status" == "Paused" ]]; then
-    icon=""  # Play
+    icon=""
 else
     echo '{"text": "", "class": "stopped"}'
     exit
 fi
 
 echo "{\"text\": \"  $title - $artist \", \"class\": \"$status\"}"
-
-# #!/bin/bash
-#
-# artist=$(playerctl metadata artist 2>/dev/null)
-# title=$(playerctl metadata title 2>/dev/null)
-# status=$(playerctl status 2>/dev/null)
-#
-# if [[ "$status" == "Playing" || "$status" == "Paused" ]]; then
-#     icon="⏵"
-#     [[ "$status" == "Paused" ]] && icon="⏸"
-#     echo "{\"text\": \"$icon $artist - $title\", \"class\": \"$status\"}"
-# else
-#     echo "{\"text\": \"\", \"class\": \"stopped\"}"
-# fi
-#
 
