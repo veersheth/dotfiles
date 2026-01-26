61c65e1c557cd1483edda67872449d4e986c742c
Veer <veerksheth@gmail.com>
2026-01-26 14:56:45 +0530

    chore nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running 'nixos-help').
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./hardware/nixos-hardware/framework/13-inch/7040-amd
     ];
 
   # Bootloader.
@@ -16,15 +15,9 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 6; # keeps 6 generations 
 
-  # Kernel parameters for better suspend and power management
-  boot.kernelParams = [ 
-    "kvm.enable_virt_at_load=0"   # virtualization(for COMP3431)
-    "amd_pstate=active"            # Better AMD CPU power management
-    "rtc_cmos.use_acpi_alarm=1"    # Better RTC handling for wakeup
-  ];
-
-  # AMD specific power management
-  boot.kernelModules = [ "amd_pstate" ];
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelModules = [ "hid_sensor_hub" ];
 
   networking.hostName = "framework-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -37,15 +30,13 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  # time.timeZone = "Australia/Sydney";
   time.timeZone = "Asia/Kolkata";
-  # services.automatic-timezoned.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_AU.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_AU.UTF-8";
     LC_MEASUREMENT = "en_AU.UTF-8";
     LC_MONETARY = "en_AU.UTF-8";
@@ -61,71 +52,11 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Power management configuration
-  services.logind = {
-    lidSwitch = "suspend";           # Suspend when lid closes
-    lidSwitchExternalPower = "suspend"; # Also suspend on AC power
-    lidSwitchDocked = "ignore";      # Don't suspend when docked
-    powerKey = "suspend";            # Suspend on power button
-    powerKeyLongPress = "poweroff";  # Long press to power off
-    extraConfig = ''
-      HandleSuspendKey=suspend
-      HandleHibernateKey=hibernate
-      IdleAction=suspend
-      IdleActionSec=15min
-    '';
-  };
-
-  # Power profiles for better battery life
-  services.power-profiles-daemon.enable = true;
-  
-  # UPower configuration for battery management
-  services.upower = {
-    enable = true;
-    percentageLow = 15;
-    percentageCritical = 5;
-    percentageAction = 3;
-    criticalPowerAction = "Hibernate";  # Hibernate when critically low
-  };
-
-  # System sleep configuration - FIXED: removed conflicting SuspendState
-  systemd.sleep.extraConfig = ''
-    HibernateDelaySec=2h
-  '';
-
-  # ACPI settings for better s2idle behavior
-  powerManagement = {
-    enable = true;
-    powertop.enable = true;
-    cpuFreqGovernor = "powersave";
-  };
-
-  # Prevent USB devices from waking the system instantly
-  services.udev.extraRules = ''
-    # Disable USB wakeup to prevent instant wake from suspend
-    ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="disabled"
-    
-    # Keep internal keyboard wake enabled (Framework specific - vendor ID 32ac)
-    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="32ac", ATTR{power/wakeup}="enabled"
-  '';
-
-  # Disable ACPI wakeup sources that cause instant wake
-  systemd.services.disable-wake-sources = {
-    description = "Disable problematic ACPI wake sources";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash -c 'echo GPP6 > /proc/acpi/wakeup; echo GP11 > /proc/acpi/wakeup; echo GP12 > /proc/acpi/wakeup; echo NHI0 > /proc/acpi/wakeup; echo NHI1 > /proc/acpi/wakeup'";
-    };
-  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us";
+    layout = "au";
     variant = "";
   };
 
@@ -151,42 +82,109 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with 'passwd'.
+  hardware.sensor.iio.enable = true; # for autobrightness
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.veer = {
     isNormalUser = true;
     description = "Veer";
-    extraGroups = [ "networkmanager" "wheel" ];
+    # extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "video" ]; 
     packages = with pkgs; [
+      # Apps
       rquickshare
-      tor-browser
-      # davinci-resolve
-      ulauncher
-      mixxx
-      figma-linux
-      spotify
-      gimp3-with-plugins
       brave
-      vscode
-      obs-studio
+      gimp3
+      spotify
+      tor-browser
+      obsidian
+      alacritty
+      zathura
       lazygit
-      typst
-      obsidian      	
       mpv
-      pnpm_9 
-      # nodejs_24 
-      nodejs_20 
-      zathura 
+      ghostty
+      davinci-resolve
+      vscode
+      livecaptions
+      obs-studio
       gnome-extension-manager
-      alacritty kitty
+      shotcut
+
+      # CLI
       sshfs
-      sox
+      typst
+      pnpm_9 nodejs_20
+
+      # GNOME Extensions
+      gnomeExtensions.bluetooth-battery-meter 
+      gnomeExtensions.live-captions-assistant
+      gnomeExtensions.gsconnect 
+      gnomeExtensions.impatience 
+      gnomeExtensions.just-perfection 
+      gnomeExtensions.caffeine
+      gnomeExtensions.appindicator
     ];
   };
 
+  users.defaultUserShell=pkgs.zsh; 
+  users.users.veer.shell = pkgs.zsh;
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      inter
+      helvetica-neue-lt-std
+      junicode
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+      nerd-fonts.iosevka
+      agave
+      maple-mono.NF
+      
+      noto-fonts-emoji
+      noto-fonts-color-emoji
+      twitter-color-emoji
+    ];
+    
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" "Twitter Color Emoji" ];
+      };
+    };
+  };
+
+  programs = {
+    firefox = {
+      enable = true;
+    };
+    # steam = {
+    #   enable = true;
+    # };
+    zsh = {
+      enable = true;
+      shellAliases = { cat = "bat"; };
+    };
+    # hyprland = {
+    #   enable = true;
+    #   xwayland.enable = true;
+    # };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    kdeconnect = {
+      enable = true;
+      package = pkgs.gnomeExtensions.gsconnect;
+    };
+  };
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    bibata-cursors
+    brightnessctl
     python313 python313Packages.pip
     bat
     pkg-config
@@ -209,72 +207,11 @@
     fd
     ripgrep
     gnome-tweaks
-    cargo rustup rustc
-
-    # for hypr
-    wl-clipboard copyq brightnessctl waybar dunst ulauncher hyprpaper hypridle 
-    hyprlock hyprsunset hyprpicker hyprshot rofi-wayland hyprcursor cliphist
-    networkmanagerapplet blueman ashell
+    cargo rustc
   ];
-
-  environment.sessionVariables = {
-    XCURSOR_THEME = "Bibata-Modern-Ice"; 
-    XCURSOR_SIZE = "24"; 
-  };
 
   environment.etc."keyd/default.conf" = {
     source = files/keyd/default.conf;
-  };
-
-  users.defaultUserShell=pkgs.zsh; 
-  users.users.veer.shell = pkgs.zsh; 
-  
-  programs = {
-    firefox = {
-      enable = true;
-    };
-    steam = {
-      enable = true;
-    };
-    zsh = {
-      enable = true;
-      shellAliases = { cat = "bat"; };
-    };
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
-  };
-
-  fonts = {
-    enableDefaultPackages = true;
-
-    packages = with pkgs; [
-      inter
-      helvetica-neue-lt-std
-      junicode
-
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.fira-code
-      nerd-fonts.iosevka
-      agave
-      maple-mono.NF
-    ];
-
-    fontconfig = {
-      enable = true;
-
-      defaultFonts = {
-        sansSerif = [ "Inter" "Helvetica Neue" ];
-        serif = [ "Junicode" ];
-        monospace = [ "JetBrainsMono Nerd Font" ];
-        emoji = [ "Noto Color Emoji" ];
-      };
-    };
   };
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -285,24 +222,23 @@
   #   enableSSHSupport = true;
   # };
 
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    # missing dynamic libraries for unpackaged programs here
-  ];
-
   # List services that you want to enable:
+
   services = {
-    timesyncd.enable = true;
+    # timesyncd.enable = true;
     fwupd.enable = true;
     keyd.enable = true;
     fprintd.enable = true;
-    # flatpak.enable = true;
+    flatpak.enable = true;
+    upower = {
+      enable = true;
+      criticalPowerAction = "Hibernate";
+      percentageLow = 10;
+      percentageCritical = 5;
+      percentageAction = 2;
+    };
   };
 
-  # Virtualization for COMP3431
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "veer" ];
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -315,9 +251,11 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
+
 }
+
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,25 +8,17 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3945c137-dd29-4816-979f-53dba767b996";
+    { device = "/dev/disk/by-uuid/547219f4-a433-4574-99ee-27316db356b4";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6534-6533";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/11db4c58-571a-4227-b411-c34329df3d6f"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
